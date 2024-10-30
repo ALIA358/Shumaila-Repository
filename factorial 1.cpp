e2676354fb68ae887521d90e9d073cb759b662b9
ALIA358 <shumaila0okm@gmail.com>
2024-10-29 22:03:31 -0700

    Update factorial 1.cpp

--- factorial 1.cpp
+++ factorial 1.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int main()
 {
 	
-int a,s;
+int a;
+long s;
 cout<<"enter a no to find fictorial"<<endl;
 cin>>a;
 
@@ -12,5 +13,6 @@ for(int i=1; i<a; i++)
 		s=i*a ;
 }
 
-	cout<<"Factorial of "<<a<<" is "<<s;	
+	cout<<"Factorial of "<<a<<" is "<<s;
+return 0;
 }
